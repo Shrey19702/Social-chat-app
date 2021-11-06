1c58805781182af1c142c047a643c2bf86e1ebe7
shrey19702 <shrey19702@gmail.com>
2021-11-06 23:43:55 +0530

    add create post router , controller <adding data into mongo>

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,21 @@
+const Post = require('../models/post_model');
+
 module.exports.postwall=function (req, res){
     res.render('postwall', {
         "title": "postwall"
     });
+}
+
+module.exports.createPost = function(req, res){
+    Post.create(
+        {
+            content : req.body.content,
+            user : req.user._id, 
+        },
+        function (error , post){
+            if(error){console.log('error in creating a new post'); return;}
+
+            return res.redirect('back');
+        }
+    );
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ const router = express.Router(); // using router from express to handle request 
 const postController = require('../controllers/posts_controller'); // controller to call for request
 
 router.get('/postwall', postController.postwall);
+router.post('/createPost', postController.createPost);
 
 module.exports = router;
