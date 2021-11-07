b91e8e5c919b0c8d116155e59e7cc405eed68821
shrey19702 <shrey19702@gmail.com>
2021-11-07 16:36:13 +0530

    add authentication to posting

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,16 +1,6 @@
 const Post = require('../models/post_model');
 
 module.exports.postwall=function (req, res){
-    // Post.find({},
-    //     function(error, post){
-    //         if(error){console.log('erorr in printing posts'); return;}
-    //         return res.render('postwall', {
-    //             "title": "postwall",
-    //             "posted": post
-    //         });
-    //     }
-    // );
-
     Post.find({}).populate('user').exec(
         function(error, post){
             if(error){console.log('erorr in printing posts'); return;}
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+
 const router = express.Router(); // using router from express to handle request (like app) 
 
 const postController = require('../controllers/posts_controller'); // controller to call for request
 
 router.get('/postwall', postController.postwall);
-router.post('/createPost', postController.createPost);
+router.post('/createPost' ,passport.checkAuthentication ,postController.createPost);
 
 module.exports = router;
